69e74d453c21ca0ab0b69224c28d1c666608d0ac
Carole JEGOU <carole.jegou41@sfr.fr>
2023-12-01 09:30:20 +0100

    steps 4&5

--- assets/script.js
+++ assets/script.js
@@ -21,17 +21,38 @@ console.log(slides);
 
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
-const dots = document.querySelector(".dot");
-const banner = document.querySelector(".banner-img");
+const banner_dots = document.querySelector(".dots");
+const banner_img = document.querySelector(".banner-img");
+const banner_tagLine = document.querySelector("#banner p");
 
 let currentIndex = 0;
 
-arrow_left.addEventListener("click", function () {
-  currentIndex = currentIndex - 1;
+arrow_left.addEventListener("click", () => {
+  dots[currentIndex].classList.remove("dot_selected");
+  currentIndex - 1 < 0 ? (currentIndex = slides.length - 1) : currentIndex--;
   console.log("click left", currentIndex);
+  slides_update();
 });
 
-arrow_right.addEventListener("click", function () {
-  currentIndex = currentIndex + 1;
+arrow_right.addEventListener("click", () => {
+  dots[currentIndex].classList.remove("dot_selected");
+  currentIndex + 1 > slides.length - 1 ? (currentIndex = 0) : currentIndex++;
   console.log("click right", currentIndex);
+  slides_update();
 });
+
+slides.forEach(() => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  banner_dots.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".dot");
+
+function slides_update() {
+  dots[currentIndex].classList.add("dot_selected");
+  banner_img.src = `./assets/images/slideshow/${slides[currentIndex].image}`;
+  banner_tagLine.innerHTML = slides[currentIndex].tagLine;
+}
+
+slides_update();
